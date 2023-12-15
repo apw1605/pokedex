0a223ffa9dbee459b6c399715858e8b6e40540b2
Ing. Antonio Peña Wong <apw1605@gmail.com>
2023-12-15 10:12:38 +0100

    Es aqui donde se escribe

--- src/components/common/pokemonCards/basic/index.tsx
+++ src/components/common/pokemonCards/basic/index.tsx
@@ -17,7 +17,7 @@ export const BasicCard = ({ name, image }: BasicCardProps) => {
         <Image
           src={`${defaultImageUrl}${image}`}
           width='150'
-          height='80'
+          height='85'
           alt=''
           className='scale-75'
           priority
